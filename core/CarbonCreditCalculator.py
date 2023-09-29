309dc14a63e2ce720f37f094c86598e9394bf461
Jayant Parida <jayant2014@users.noreply.github.com>
2023-09-29 18:39:51 +0530

    Update CarbonCreditCalculator.py

--- core/CarbonCreditCalculator.py
+++ core/CarbonCreditCalculator.py
@@ -2,9 +2,18 @@ class CarbonCreditCalculator:
     def __init__(self):
         # Conversion factors for carbon credits estimation (hypothetical values)
         self.conversion_factors = {
-            'plastic': 0.01,  # Dummy conversion factor
-            'glass': 0.02,    # Dummy conversion factor
-            'clothes': 0.03,  # Dummy conversion factor
+            'plastic': {
+                'recyclability': 0.7,  # Hypothetical recyclability factor (0 to 1)
+                'emissions_saved_per_kg': 2.5,  # Hypothetical emissions saved per kg (kg CO2e)
+            },
+            'glass': {
+                'recyclability': 0.9,  # Hypothetical recyclability factor (0 to 1)
+                'emissions_saved_per_kg': 0.5,  # Hypothetical emissions saved per kg (kg CO2e)
+            },
+            'clothes': {
+                'recyclability': 0.5,  # Hypothetical recyclability factor (0 to 1)
+                'emissions_saved_per_kg': 3.0,  # Hypothetical emissions saved per kg (kg CO2e)
+            },
         }
 
     def calculate_carbon_credits(self, material_type, quantity):
